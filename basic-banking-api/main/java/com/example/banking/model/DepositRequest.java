af898b10164ad6ce75142accc6ea3628b894ddd7
Riya Gupta <98845311+RiyaaGupta02@users.noreply.github.com>
2025-06-03 18:27:40 +0530

    Create DepositRequest.java
    Deposit Request file here

---
+++ basic-banking-api/main/java/com/example/banking/model/DepositRequest.java
@@ -0,0 +1,13 @@
+package com.bankapp.bankapp.model;
+
+public class DepositRequest {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
