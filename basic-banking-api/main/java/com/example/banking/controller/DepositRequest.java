ea4d87130463472d8ec6e27fe946a1ad19226ef7
Riya Gupta <98845311+RiyaaGupta02@users.noreply.github.com>
2025-06-03 18:26:08 +0530

    Update DepositRequest.java
    Instead of here its at model

--- basic-banking-api/main/java/com/example/banking/controller/DepositRequest.java
+++ basic-banking-api/main/java/com/example/banking/controller/DepositRequest.java
@@ -1,11 +1,1 @@
-public class DepositRequest {
-    private double amount;
 
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-}
